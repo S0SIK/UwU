099ef9cc63e58668a23f7ccc487f36fd85308432
S0SIK <sosik97@wp.pl>
2023-05-13 23:15:24 +0200

    Zły

--- Punkty/script.js
+++ Punkty/script.js
@@ -8,33 +8,63 @@ let updatedAllDatatext = [];
 let Klient = []
 let Pracownik = []
 let index = 0
-for (const [player, value] of Object.entries(text.sort())) {
+for (const [player, value] of Object.entries(text)) {
     
     res = index++
 updatedAllDatatext[res] = text[res].split(` `)
 }
 for (const [player, value] of Object.entries(updatedAllDatatext)) {
-if (value[2] == `|`) {
-    Klient[`${value[0]} ${value[1]}`] = 0
-  } 
+if (value[1] == `|`) {
+    Klient[`${player} ${value[0]}`] = {
+        "Id": player
+    }
+  }else if (value[2] == `|`) {
+    Klient[`${player} ${value[0]} ${value[1]}`] = {
+        "Id": player
+    }
+  }else if (value[3] == `|`) {
+    Klient[`${player} ${value[0]} ${value[1]} ${value[2]}`] = {
+        "Id": player
+    }
+  }else if (value[4] == `|`) {
+    Klient[`${player} ${value[0]} ${value[1]} ${value[2]} ${value[3]}`] = {
+        "Id": player
+    }
+  }else if (value[5] == `|`) {
+    Klient[`${player} ${value[0]} ${value[1]} ${value[2]} ${value[3]} ${value[4]}`] = {
+        "Id": player
+    }
+  }else if (value[1] == `—`) {
+    Pracownik[`${player} ${value[0]}`] = {
+        "Id": player
+    }
+  }else if (value[2] == `—`) {
+    Pracownik[`${player} ${value[0]} ${value[1]}`] = {
+        "Id": player
+    }
+  }else if (value[3] == `—`) {
+    Pracownik[`${player} ${value[0]} ${value[1]} ${value[2]}`] = {
+        "Id": player
+    }
+  }else if (value[4] == `—`) {
+    Pracownik[`${player} ${value[0]} ${value[1]} ${value[2]} ${value[3]}`] = {
+        "Id": player
+    }
+  }else if (value[5] == `—`) {
+    Pracownik[`${player} ${value[0]} ${value[1]} ${value[2]} ${value[3]} ${value[4]}`] = {
+        "Id": player
+    }
+  }
   Klient.sort();
 } 
 
-for (const [player, value] of Object.entries(updatedAllDatatext)) {
-    if (value[2] == `—`) {
-        Pracownik[`${value[0]} ${value[1]}`] = 0
-      }
-    }
-console.log(Klient)
 for (const [player, value] of Object.entries(Klient)) {
     const htmKlient = `${player} | ${value}\n`;
-    console.log(htmKlient)
     const blipKlient = document.getElementById("Klient")
     blipKlient.insertAdjacentHTML("beforeend", htmKlient);
 }
 for (const [player, value] of Object.entries(Pracownik)) {
     const htmPracownik = `${player} | ${value}\n`;
-    console.log(htmPracownik)
     const blipPracownik = document.getElementById("Pracownik")
     blipPracownik.insertAdjacentHTML("beforeend", htmPracownik);
 }
