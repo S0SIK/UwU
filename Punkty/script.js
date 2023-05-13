38adc8886048f10b0585a746d2d733435c777b36
S0SIK <sosik97@wp.pl>
2023-05-13 22:15:38 +0200

    Sort A-Z

--- Punkty/script.js
+++ Punkty/script.js
@@ -1,7 +1,41 @@
+function sortNum (a, b) {
+    return a - b;
+  }
+
 function prettyPrint() {
-    var dataTxt = document.getElementById('Dane').value;
-    console.log(dataTxt)
-
-
+const text = document.getElementById('Dane').value.split(`\n`);
+let updatedAllDatatext = [];
+let Klient = []
+let Pracownik = []
+let index = 0
+for (const [player, value] of Object.entries(text.sort())) {
     
+    res = index++
+updatedAllDatatext[res] = text[res].split(` `)
+}
+for (const [player, value] of Object.entries(updatedAllDatatext)) {
+if (value[2] == `|`) {
+    Klient[`${value[0]} ${value[1]}`] = 0
+  } 
+  Klient.sort();
+} 
+
+for (const [player, value] of Object.entries(updatedAllDatatext)) {
+    if (value[2] == `—`) {
+        Pracownik[`${value[0]} ${value[1]}`] = 0
+      }
+    }
+console.log(Klient)
+for (const [player, value] of Object.entries(Klient)) {
+    const htmKlient = `${player} | ${value}\n`;
+    console.log(htmKlient)
+    const blipKlient = document.getElementById("Klient")
+    blipKlient.insertAdjacentHTML("beforeend", htmKlient);
+}
+for (const [player, value] of Object.entries(Pracownik)) {
+    const htmPracownik = `${player} | ${value}\n`;
+    console.log(htmPracownik)
+    const blipPracownik = document.getElementById("Pracownik")
+    blipPracownik.insertAdjacentHTML("beforeend", htmPracownik);
+}
 }
